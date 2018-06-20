904bec5992db74845f9416d8aaa13b10312708d7
violennz <violennz@protonmail.com>
2018-06-20 14:07:49 -0300

    version V1.0

---
+++ Serve.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	ServeFiles()
+}
+
+// References: https://gist.github.com/paulmach/7271283
+func ServeFiles() {
+	ShowName()
+	port := flag.String("p", "8100", "Serve")
+	directory := flag.String("d", ".", "VIOL3N Directory")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+	log.Printf("\n\nServing ViOL3Nz directory %s on HTTP port: %s\n", *directory, *port)
+	fmt.Println("\nClose this window to stop.")
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
+
+func ShowName() {
+	fmt.Println("")
+	fmt.Println("██╗   ██╗██╗ ██████╗ ██╗     ██████╗ ███╗   ██╗")
+	fmt.Println("██║   ██║██║██╔═══██╗██║     ╚════██╗████╗  ██║")
+	fmt.Println("██║   ██║██║██║   ██║██║      █████╔╝██╔██╗ ██║")
+	fmt.Println("╚██╗ ██╔╝██║██║   ██║██║      ╚═══██╗██║╚██╗██║")
+	fmt.Println(" ╚████╔╝ ██║╚██████╔╝███████╗██████╔╝██║ ╚████║")
+	fmt.Println("  ╚═══╝  ╚═╝ ╚═════╝ ╚══════╝╚═════╝ ╚═╝  ╚═══╝")
+}
